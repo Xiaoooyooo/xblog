1e7faf5e6083872b0414b5dd59463307b022a4df
Xiaoooyooo <xiaoooyooo@gmail.com>
2023-11-29 17:59:47 +0800

    add deploy action

--- build.js
+++ build.js
@@ -2,12 +2,14 @@
 const { exec } = require("child_process");
 
 function execAsync(cmd) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     exec(cmd, (error, stdout, stderr) => {
       if (error) {
         console.log(error);
+        reject(error);
+      } else {
+        resolve(stdout ? stdout : stderr);
       }
-      resolve(stdout ? stdout : stderr);
     });
   });
 }
@@ -16,11 +18,10 @@ async function build() {
   await Promise.all([
     execAsync("yarn build:app"),
     execAsync("yarn build:server"),
-    execAsync("cp -r ./server/prisma ./build/server/"),
-    execAsync("cp package.json ./build/"),
-    execAsync("cp .env.sample ./build/"),
   ]);
-
+  await execAsync("cp -r ./server/prisma ./build/server/");
+  await execAsync("cp package.json ./build/");
+  await execAsync("cp .env.sample ./build/");
   console.log("done");
 }
 
